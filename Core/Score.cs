4e07219b45002b852201a013691a410c6bf33bb1
sanosuguru <sa.50.00.no.riku@gmail.com>
2025-02-14 23:49:25 +0900

    Score構造体に減算演算子を追加し、ToStringメソッドをオーバーライドしてフォーマットを改善

--- Core/Score.cs
+++ Core/Score.cs
@@ -2,6 +2,10 @@ namespace Blazor2048.Core;
 
 public readonly record struct Score(int Value)
 {
-    public static Score operator +(Score a, Score b) => new(a.Value + b.Value);
     public static Score Zero => new(0);
+
+    public static Score operator +(Score a, Score b) => new(a.Value + b.Value);
+    public static Score operator -(Score a, Score b) => new(a.Value - b.Value);
+
+    public override string ToString() => Value.ToString("N0");
 }
