c05d3fb4b68f1c3613b16a51434f959ffd7b3e13
sanosuguru <sa.50.00.no.riku@gmail.com>
2025-02-14 23:53:32 +0900

    GameExceptionクラスにコンストラクタを追加し、内部例外を処理できるように改善

--- Core/GameException.cs
+++ Core/GameException.cs
@@ -1,5 +1,7 @@
 namespace Blazor2048.Core;
 
-public class GameException(string message) : Exception(message)
+public class GameException : Exception
 {
+    public GameException(string message) : base(message) { }
+    public GameException(string message, Exception inner) : base(message, inner) { }
 }
